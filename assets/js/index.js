c77dd36ed86d153e7d16fff4cbe2cf65cf94db44
Ehsan Yousefzadeh-Asl-Miandoab <ehsanyusefzadehasl@gmail.com>
2024-08-22 13:58:06 +0200

    added navigation bar to easier navigate through the resume :)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.body.classList.add("dark");
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
